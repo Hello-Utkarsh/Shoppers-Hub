a0002d610b00f93537265785b4cb9c4d44d77b26
Utkarsh Patel <pateluttkarsh697@gmail.com>
2022-11-27 12:03:58 +0530

    modified the filter and search section

--- JsFiles/script.js
+++ JsFiles/script.js
@@ -1,40 +1,39 @@
-function myfunction1(){
-    if (document.getElementById("dropdown1").style.display == 'none') {
-        document.getElementById("dropdown1").style.display = 'block'
-        document.getElementById("dropdown1").style.position = 'fixed'
+function myfunction() {
+  if (document.getElementById("dropdown").style.display == 'none') {
+    document.getElementById("dropdown").style.display = 'flex'
   }
-  else{
-    document.getElementById("dropdown1").style.display = 'none'
-  }
-}
-
-function myfunction2(){
-    if (document.getElementById("dropdown2").style.display == 'none') {
-        document.getElementById("dropdown2").style.display = 'block'
-        document.getElementById("dropdown2").style.position = 'fixed'
-  }
-  else{
-    document.getElementById("dropdown2").style.display = 'none'
+  else {
+    document.getElementById("dropdown").style.display = 'none'
   }
 }
 
-function myfunction3(){
-    if (document.getElementById("dropdown3").style.display == 'none') {
-        document.getElementById("dropdown3").style.display = 'block'
-        document.getElementById("dropdown3").style.position = 'fixed'
-  }
-  else{
-    document.getElementById("dropdown3").style.display = 'none'
-  }
-}
+// function myfunction2(){
+//     if (document.getElementById("dropdown2").style.display == 'none') {
+//         document.getElementById("dropdown2").style.display = 'block'
+//         document.getElementById("dropdown2").style.position = 'fixed'
+//   }
+//   else{
+//     document.getElementById("dropdown2").style.display = 'none'
+//   }
+// }
 
-function myfunction4(){
-    if (document.getElementById("dropdown4").style.display == 'none') {
-        document.getElementById("dropdown4").style.display = 'block'
-        document.getElementById("dropdown4").style.position = 'fixed'
-  }
-  else{
-    document.getElementById("dropdown4").style.display = 'none'
-  }
-}
+// function myfunction3(){
+//     if (document.getElementById("dropdown3").style.display == 'none') {
+//         document.getElementById("dropdown3").style.display = 'block'
+//         document.getElementById("dropdown3").style.position = 'fixed'
+//   }
+//   else{
+//     document.getElementById("dropdown3").style.display = 'none'
+//   }
+// }
+
+// function myfunction4(){
+//     if (document.getElementById("dropdown4").style.display == 'none') {
+//         document.getElementById("dropdown4").style.display = 'block'
+//         document.getElementById("dropdown4").style.position = 'fixed'
+//   }
+//   else{
+//     document.getElementById("dropdown4").style.display = 'none'
+//   }
+// }
 
