c37d306009af4ec845ac0a331fca889ac67448a9
Utkarsh Patel <pateluttkarsh697@gmail.com>
2022-11-27 23:53:26 +0530

    modified the products section

--- JsFiles/script.js
+++ JsFiles/script.js
@@ -1,4 +1,6 @@
+
 function myfunction() {
+  document.getElementById("dropdown").style.display == 'flex'
   if (document.getElementById("dropdown").style.display == 'none') {
     document.getElementById("dropdown").style.display = 'flex'
   }
@@ -6,34 +8,4 @@ function myfunction() {
     document.getElementById("dropdown").style.display = 'none'
   }
 }
-
-// function myfunction2(){
-//     if (document.getElementById("dropdown2").style.display == 'none') {
-//         document.getElementById("dropdown2").style.display = 'block'
-//         document.getElementById("dropdown2").style.position = 'fixed'
-//   }
-//   else{
-//     document.getElementById("dropdown2").style.display = 'none'
-//   }
-// }
-
-// function myfunction3(){
-//     if (document.getElementById("dropdown3").style.display == 'none') {
-//         document.getElementById("dropdown3").style.display = 'block'
-//         document.getElementById("dropdown3").style.position = 'fixed'
-//   }
-//   else{
-//     document.getElementById("dropdown3").style.display = 'none'
-//   }
-// }
-
-// function myfunction4(){
-//     if (document.getElementById("dropdown4").style.display == 'none') {
-//         document.getElementById("dropdown4").style.display = 'block'
-//         document.getElementById("dropdown4").style.position = 'fixed'
-//   }
-//   else{
-//     document.getElementById("dropdown4").style.display = 'none'
-//   }
-// }
 
